77d111c8532f0aad91fec08d495ffafe24a6a7b3
JovemHero <68248737+JovemHero@users.noreply.github.com>
2021-03-11 23:07:58 -0300

    Create queue.js

---
+++ commands/queue.js
@@ -0,0 +1,34 @@
+const { MessageEmbed } = require("discord.js");
+
+const { COLOR } = require("../config.json");
+
+module.exports = {
+  name: "queue",
+  aliases: ['lista', 'fila'],
+  execute: (client, message, args) => {
+    let embed = new MessageEmbed().setColor(COLOR);
+    const { channel } = message.member.voice;
+
+    if (!channel) {
+      embed.setAuthor("Você precisa estar em um canal de voz!");
+      return message.channel.send(embed);
+    }
+
+    const serverQueue = message.client.queue.get(message.guild.id);
+
+    if (!serverQueue) {
+      embed.setAuthor("Não há nada na fila!");
+      return message.channel.send(embed);
+    }
+
+    embed.setDescription(
+      `${serverQueue.songs
+        .map((song, index) => index + 1 + ". " + song.title)
+        .join("\n\n")}`,
+      { split: true }
+    );
+    embed.setThumbnail(client.user.displayAvatarURL())
+    
+    message.channel.send(embed);
+  }
+};
